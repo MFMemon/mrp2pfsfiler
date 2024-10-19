6b6c53fafaee490eefcf80a560cefda535f0069d
MFMemon <mohammadfaizan91@gmail.com>
2024-10-19 21:55:27 +0400

    first remote push commit

---
+++ runfsserver.sh
@@ -0,0 +1,15 @@
+apt-get update && apt-get install -y apt-transport-https ca-certificates curl
+
+SEAWEEDFS_VER=3.77
+
+# choose either URL
+DOWNLOAD_URL=https://github.com/seaweedfs/seaweedfs/releases/download
+
+rm -f /tmp/seaweedfs-${SEAWEEDFS_VER}-linux-amd64_full.tar.gz
+rm -rf /tmp/seaweedfs && mkdir -p /tmp/seaweedfs
+
+curl -L ${DOWNLOAD_URL}/${SEAWEEDFS_VER}/linux_amd64_full.tar.gz -o /tmp/seaweedfs-${SEAWEEDFS_VER}-linux-amd64_full.tar.gz
+tar xzvf /tmp/seaweedfs-${SEAWEEDFS_VER}-linux-amd64_full.tar.gz -C /tmp/seaweedfs
+rm -f /tmp/seaweedfs-${SEAWEEDFS_VER}-linux-amd64_full.tar.gz
+
+/tmp/seaweedfs/weed filer -port=$1 -master=$2
